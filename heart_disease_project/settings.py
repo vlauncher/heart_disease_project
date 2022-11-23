5b7298fd1bb7acec743779547ca3a39bb94d8ec9
vlauncher <v2dent@gmail.com>
2022-11-23 20:58:02 +0100

    made Changes

--- heart_disease_project/settings.py
+++ heart_disease_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'rest_framework',
     'djoser',
     "corsheaders",
@@ -87,7 +88,7 @@ ROOT_URLCONF = 'heart_disease_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / '../client/build'],
+        'DIRS': [BASE_DIR / './client/build'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -149,7 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR/'../client/build/static']
+STATICFILES_DIRS = [BASE_DIR/'./client/build/static']
 STATIC_ROOT = (BASE_DIR/'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
