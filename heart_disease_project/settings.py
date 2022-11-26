2888db33e24ce111b27614203afdcdd086946282
vlauncher <v2dent@gmail.com>
2022-11-26 17:20:07 +0100

    added

--- heart_disease_project/settings.py
+++ heart_disease_project/settings.py
@@ -164,4 +164,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000','https://heartpredv1.onrender.com']
